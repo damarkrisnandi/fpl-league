1cf4a528ae0630209d29b754d799d13f6c5d11e2
damar <damar@gitlab.lab>
2023-08-30 17:19:24 +0700

    fix lagi

--- components/layout/divisionBound.tsx
+++ components/layout/divisionBound.tsx
@@ -1,4 +1,4 @@
-export default function DivisionBound(props) {
+export default function DivisionBound(props: any) {
     const { message, color } = props;
     return (
         <a className={`flex flex-col items-center justify-center ${color} border border-gray-200 rounded-lg shadow md:flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mb-2`}>
