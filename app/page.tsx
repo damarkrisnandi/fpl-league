10e751b9c2f0cceafed14dfcabd368f7ba57f474
Damar Krisnandi Ramdan <damarkrisnandi1202@gmail.com>
2023-08-31 17:59:15 +0700

    Update page.tsx motw

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,12 @@ export default async function Home() {
   
   let leagueA: any = (await getLeagueData(leagueAId, `${page}`));
   standingsA = [...leagueA.standings.results];
+  
+  while (leagueA.standings.has_next) {
+    page++;
+    leagueA = (await getLeagueData(leagueAId, `${page}`));
+    standingsA = [...standingsA, ...leagueA.standings.results];
+  }
 
   let leagueB: any = (await getLeagueData(leagueBId, `${page}`));
   standingsB = [...leagueB.standings.results];
@@ -28,6 +34,7 @@ export default async function Home() {
   const dataA = standingsA && standingsA.length > 0 && standingsA.find(o => o.rank === 1);
   const dataB = standingsB && standingsB.length > 0 && standingsB.find(o => o.rank === 1);
   const dataSuper = standingsSuper && standingsSuper.length > 0 && standingsSuper.find(o => o.rank === 1);
+  const dataMotwA = standingsA && standingsA.length > 0 && standingsA.find(o => o.event_total === Math.max(...standingsA.map(a => a.standings.results.event_total)));
   return (
     <main className='w-11/12 m-auto flex flex-col items-center pt-24 pb-24'>
       <GameWeek></GameWeek>
@@ -41,7 +48,16 @@ export default async function Home() {
           points={dataA ? dataA.total : 0}
           id={dataA ? dataA.entry : 'N/A'}
         />
-
+      <CardSummary
+          // imgUrl={urlImageAccess(elements[Math.floor(Math.random() * 500)].photo)}
+          league='MOTW League A'
+          position={1}
+          last_rank={dataMotwA ? dataMotwA.last_rank : 1}
+          team={dataMotwA ? dataMotwA.entry_name : 'N/A'}
+          manager={dataMotwA ? dataMotwA.player_name : 'N/A'}
+          points={dataMotwA ? dataMotwA.total : 0}
+          id={dataMotwA ? dataMotwA.entry : 'N/A'}
+        />
     <CardSummary 
         // imgUrl={urlImageAccess(elements[Math.floor(Math.random() * 500)].photo)}
         league='League B'
