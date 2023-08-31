4bc8d3db607fcbc44535f7275c10406dfbf045d4
damar <damar@gitlab.lab>
2023-08-31 14:08:09 +0700

    fix props

--- components/shared/playerCard.tsx
+++ components/shared/playerCard.tsx
@@ -5,7 +5,7 @@ export default function PlayerCard(props: any) {
     return (
         <div>
 
-            <div href="#" className="items-center w-16 m-0.5">
+            <div className="items-center w-16 m-0.5">
                 <Image className="w-full h-full p-0 pb-0 rounded-t-lg md:rounded-none md:rounded-l-lg" src={imgUrl} alt="" width={150} height={150}/>
                 <div className="p-0 flex flex-col justify-center items-center bg-white border border-gray-200 shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <p className="text-xs font-light tracking-tight text-gray-900 dark:text-white whitespace-nowrap text-ellipsis overflow-hidden w-full text-center">{ name }</p>
