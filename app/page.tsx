47d7d31770b2c4dd8498f1a8828339a419577f1b
Damar Krisnandi Ramdan <damarkrisnandi1202@gmail.com>
2023-09-02 06:11:33 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import MotwCard from '@/components/layout/MotwCard';
 import GameWeek from '@/components/shared/GameWeek';
 import { getBootstrap, getLeagueData, getFixtures } from '@/services'
 import { urlImageAccess } from '@/utils';
-import { useRouter } from 'next/navigation'
 
 export default async function Home() {
   let page = 1;
@@ -41,9 +40,7 @@ export default async function Home() {
   const dataB = standingsB && standingsB.length > 0 && standingsB.find(o => o.rank === 1);
   const dataSuper = standingsSuper && standingsSuper.length > 0 && standingsSuper.find(o => o.rank === 1);
   const dataMotwA = standingsA && standingsA.length > 0 && standingsA.find(o => o.event_total === Math.max(...standingsA.map(a => a.event_total)));
-  
-  const router = useRouter()
-  router.refresh()
+
   
   return (
     <main className='w-11/12 m-auto flex flex-col items-center pt-24 pb-24'>
