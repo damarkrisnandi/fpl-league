dac627c3dead68b8d13b4edce7ccebcccff956ea
Damar Krisnandi Ramdan <damarkrisnandi1202@gmail.com>
2023-09-03 08:14:35 +0700

    Update index.ts

--- pages/api/[...slug]/index.ts
+++ pages/api/[...slug]/index.ts
@@ -4,7 +4,7 @@ import NextCors from "nextjs-cors";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     
-    fetch(`${API_URL}${req.url}${req.url?.includes('?') ? '' : '/'}`).then(data => {
+    fetch(`${API_URL}${req.url}${req.url?.includes('?') ? '' : '/'}`, { next: { revalidate: 900 } }).then(data => {
         data.json().then(data => {
             // createResponseBody(res, data);
             res.json({ ...data });
